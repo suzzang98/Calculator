e739e6701df5a73c376378fbbd529707ca058f0b
suzzang98 <hyeonnee98@gmail.com>
2023-07-18 14:14:53 +0900

    Lv1 초안

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,4 +1,44 @@
 class Calculator {
+    var result: Double? = nil
+    var oper: String? = nil
+    var firstNumber : Double = 0
+    var secondNumber : Double = 0
     
+    init (){
+
+    }
+
+    func calculate(oper: String, firstNumber: Double, secondNumber: Double)-> Double?{
+        if oper == "+" {
+            result = firstNumber + secondNumber
+        }
+        else if oper == "-" {
+            result = firstNumber - secondNumber
+        }
+        else if oper == "*" {
+            result = firstNumber * secondNumber
+        }
+        else if oper == "/" {
+            result = firstNumber / secondNumber
+        }
+        else{
+            print("정확한 연산자를 입력하세요")
+            result = nil
+        }
+        return result
+
+    }
 }
-print("HI")
+
+let cal = Calculator()
+let addResult = cal.calculate(oper: "+", firstNumber: 10, secondNumber: 5)
+let subtractResult = cal.calculate(oper: "-", firstNumber: 10, secondNumber: 5)
+let multiplyResult = cal.calculate(oper: "*", firstNumber: 10, secondNumber: 5)
+let divideResult = cal.calculate(oper: "/", firstNumber: 10, secondNumber: 5)
+let errorResult = cal.calculate(oper: " ", firstNumber: 10, secondNumber: 5)
+
+print("addResult : \(addResult)")
+print("subtractResult : \(subtractResult)")
+print("multiplyResult : \(multiplyResult)")
+print("divideResult : \(divideResult)")
+print("errorResult : \(errorResult)")
