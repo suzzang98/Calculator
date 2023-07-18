502c040badbd20f5db35ee4cef6ae7b2ed8ea5a4
suzzang98 <hyeonnee98@gmail.com>
2023-07-18 14:39:18 +0900

    부정확한 연산자와 나눗셈 일부 수정

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,14 +1,11 @@
 class Calculator {
-    var result: Double? = nil
-    var oper: String? = nil
-    var firstNumber : Double = 0
-    var secondNumber : Double = 0
-    
+
     init (){
 
     }
-
-    func calculate(oper: String, firstNumber: Double, secondNumber: Double)-> Double?{
+    
+    func calculate(oper: String, firstNumber: Double, secondNumber: Double)-> Double{
+        var result: Double = 0
         if oper == "+" {
             result = firstNumber + secondNumber
         }
@@ -19,11 +16,16 @@ class Calculator {
             result = firstNumber * secondNumber
         }
         else if oper == "/" {
-            result = firstNumber / secondNumber
+            // 나눗셈일 경우 secondNumber가 0일 경우 추가
+            if secondNumber == 0{
+                print("\n\nsecondNumber은 0일 수 없습니다.")
+            } else {
+                result = firstNumber / secondNumber
+            }
         }
         else{
-            print("정확한 연산자를 입력하세요")
-            result = nil
+            // +, -, *, / 이외의 연산자가 들어왔을 때의 출력
+            print("\n\n정확한 연산자를 입력하세요")
         }
         return result
 
@@ -35,10 +37,14 @@ let addResult = cal.calculate(oper: "+", firstNumber: 10, secondNumber: 5)
 let subtractResult = cal.calculate(oper: "-", firstNumber: 10, secondNumber: 5)
 let multiplyResult = cal.calculate(oper: "*", firstNumber: 10, secondNumber: 5)
 let divideResult = cal.calculate(oper: "/", firstNumber: 10, secondNumber: 5)
-let errorResult = cal.calculate(oper: " ", firstNumber: 10, secondNumber: 5)
 
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
 print("multiplyResult : \(multiplyResult)")
 print("divideResult : \(divideResult)")
-print("errorResult : \(errorResult)")
+
+let wrongOperator = cal.calculate(oper: " ", firstNumber: 10, secondNumber: 5)
+print("wrongOperator : \(wrongOperator)")
+
+let wrongSecondNum = cal.calculate(oper : "/", firstNumber: 10, secondNumber: 0)
+print("wrongSecondNum : \(wrongSecondNum)")
