2bdb7a3ca101118c3ece9cbd6436624de3a7f237
suzzang98 <hyeonnee98@gmail.com>
2023-07-19 09:11:08 +0900

    LV3

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,57 +1,75 @@
 class Calculator {
-
-    init (){
-
+    var firstNumber : Double
+    var secondNumber : Double
+    
+    init (firstNumber : Double, secondNumber:Double){
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
     }
     
     func calculate(oper: String, firstNumber: Double, secondNumber: Double)-> Double{
-        var result: Double = 0
-        if oper == "+" {
-            result = firstNumber + secondNumber
+        let addOperation = AddOperation()
+        let subtractOperation = SubtractOperation()
+        let multiplyOperation = MultiplyOperation()
+        let divideOperation = DivideOperation()
+        
+        if oper == "+"{
+            return addOperation.operate(firstNumber: firstNumber, secondNumber : secondNumber)
+        } else if oper == "-"{
+            return subtractOperation.operate(firstNumber: firstNumber, secondNumber : secondNumber)
+        } else if oper == "*"{
+            return multiplyOperation.operate(firstNumber: firstNumber, secondNumber : secondNumber)
+        } else if oper == "/"{
+            return divideOperation.operate(firstNumber: firstNumber, secondNumber : secondNumber)
+        } else {
+            print("\n올바르지 않은 연산자입니다.")
         }
-        else if oper == "-" {
-            result = firstNumber - secondNumber
-        }
-        else if oper == "*" {
-            result = firstNumber * secondNumber
-        }
-        else if oper == "/" {
-            // 나눗셈일 경우 secondNumber가 0일 경우 추가
-            if secondNumber == 0{
-                print("\n\nsecondNumber은 0일 수 없습니다.")
-            } else {
-                result = firstNumber / secondNumber
-            }
-        }
-        else if oper == "%" {
-            if secondNumber == 0{
-                print("\n\nsecondNumber은 0일 수 없습니다.")
-            } else {
-                // Swift에서 % 연산자는 정수형에서 밖에 사용할 수 없기 때문에, truncatingRemainder라는 메소드를 사용해서 Double 타입에서도 사용 가능하도록 수정
-                result = firstNumber.truncatingRemainder(dividingBy: secondNumber)
-            }
-        }
-        else{
-            // +, -, *, / 이외의 연산자가 들어왔을 때의 출력
-            print("\n\n정확한 연산자를 입력하세요")
-        }
-        return result
-
+        return 0
     }
 }
 
-let calculator = Calculator()
-let addResult = calculator.calculate(oper: "+", firstNumber: 10, secondNumber: 5)
-let subtractResult = calculator.calculate(oper: "-", firstNumber: 10, secondNumber: 5)
-let multiplyResult = calculator.calculate(oper: "*", firstNumber: 10, secondNumber: 5)
-let divideResult = calculator.calculate(oper: "/", firstNumber: 10, secondNumber: 5)
-let remainderResult = calculator.calculate(oper: "%", firstNumber: 10, secondNumber: 3)
+class AddOperation{
+    func operate(firstNumber: Double, secondNumber:Double) -> Double{
+        return firstNumber + secondNumber
+    }
+}
+
+class SubtractOperation{
+    func operate(firstNumber: Double, secondNumber:Double) -> Double{
+        return firstNumber - secondNumber
+    }
+}
+
+class MultiplyOperation{
+    func operate(firstNumber: Double, secondNumber:Double) -> Double{
+        return firstNumber * secondNumber
+    }
+}
+
+class DivideOperation{
+    func operate(firstNumber: Double, secondNumber:Double) -> Double{
+        if secondNumber == 0{
+            print("\nzeroDivisionError")
+            return 0
+        }
+        return firstNumber / secondNumber
+    }
+}
+
+
+
+let calculator = Calculator(firstNumber: 10, secondNumber: 2)
+
+
+let addResult = calculator.calculate(oper: "+", firstNumber: 10, secondNumber: 2)
+let subtractResult = calculator.calculate(oper: "-", firstNumber: 10, secondNumber: 2)
+let multiplyResult = calculator.calculate(oper: "*", firstNumber: 10, secondNumber: 2)
+let divideResult = calculator.calculate(oper: "/", firstNumber: 10, secondNumber: 2)
 
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
 print("multiplyResult : \(multiplyResult)")
 print("divideResult : \(divideResult)")
-print("remainderResult : \(remainderResult)")
 
 let wrongOperator = calculator.calculate(oper: " ", firstNumber: 10, secondNumber: 5)
 print("wrongOperator : \(wrongOperator)")
